f84e9879a0495941e51015c0661d2fa1cbe29d1a
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2023-11-14 19:16:26 +0200

    blogs

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,85 +1,3 @@
-// import React, { useState } from "react";
-// import { Helmet } from "react-helmet-async";
-// import "../assets/scss/_blogs.scss";
-// import "../assets/scss/base.scss";
-// import Blog from "../components/Blog";
-// import BlogData from "../data/BlogData";
-// import Filters from "../components/Filters";
-
-// const Blogs = () => {
-//   const blogsPerPage = 9;
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const [selectedFilters, setSelectedFilters] = useState([]);
-
-//   const totalBlogs = BlogData.length;
-//   const totalPages = Math.ceil(totalBlogs / blogsPerPage);
-
-//   const handleNextPage = () => {
-//     setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
-//   };
-
-//   const handlePrevPage = () => {
-//     setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-//   };
-
-//   const handleSelectFilter = (filters) => {
-//     setSelectedFilters(filters);
-//     setCurrentPage(1); // Reset currentPage when changing filters
-//   };
-
-//   const indexOfLastBlog = currentPage * blogsPerPage;
-//   const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
-
-//   const filteredBlogs =
-//     selectedFilters.length > 0 && !selectedFilters.includes("all")
-//       ? BlogData.filter((blog) => selectedFilters.includes(blog.filter))
-//       : BlogData;
-
-//   const currentBlogs = filteredBlogs.slice(indexOfFirstBlog, indexOfLastBlog);
-
-//   return (
-//     <section dir="rtl" className="blogs">
-//       <Helmet>
-//         <title>SkyzCRM</title>
-//         <meta name="description" content="אתר הבלוגים של Skyz CRM"></meta>
-//         <link rel="canonical" href="/בלוג"></link>
-//       </Helmet>
-//       <h1>Skyz Blog</h1>
-//       <p>
-//         ברוך בואכם לאתר הבלוגים של Skyz CRM. האתר כולל מאמרים מקצועיים, אשר
-//         יציידו אתכם במידע חיוני וטיפים מעניינים, בתחומי מכירות, שיווק, שירות,
-//         הצלחת לקוח ו- CRM (ניהול קשרי לקוחות). המאמרים יציגו לכם חידושים,
-//         ניתוחים ומגמות עולמיות לצורך התייעלות בכל אחד מתחומים חשובים אלו. נשמח
-//         מאוד אם תפיקו מאתר בלוגים זה את המיטב.
-//       </p>
-//       <Filters onSelectFilter={handleSelectFilter} />
-//       <div className="blogs-container">
-//         <Blog blogs={currentBlogs} />
-//       </div>
-//       <div className="pagination">
-//         <button
-//           className="forward-btn"
-//           onClick={handlePrevPage}
-//           disabled={currentPage === 1}
-//         >
-//           <span className="material-symbols-outlined">arrow_forward_ios</span>
-//         </button>
-//         <span className="page-number">{`${currentPage} / ${totalPages}`}</span>
-//         <button
-//           className="back-btn"
-//           onClick={handleNextPage}
-//           disabled={currentPage === totalPages}
-//         >
-//           <span className="material-symbols-outlined">arrow_back_ios</span>
-//         </button>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Blogs;
-
-// Blogs.js
 import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import "../assets/scss/_blogs.scss";
@@ -93,7 +11,16 @@ const Blogs = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedFilters, setSelectedFilters] = useState([]);
 
-  const totalBlogs = BlogData.length;
+  const filteredBlogs =
+    selectedFilters.length > 0
+      ? BlogData.filter(
+          (blog) =>
+            blog.filters &&
+            blog.filters.some((filter) => selectedFilters.includes(filter))
+        )
+      : BlogData;
+
+  const totalBlogs = filteredBlogs.length; // Update to get the total number of filtered blogs
   const totalPages = Math.ceil(totalBlogs / blogsPerPage);
 
   const handleNextPage = () => {
@@ -111,15 +38,6 @@ const Blogs = () => {
 
   const indexOfLastBlog = currentPage * blogsPerPage;
   const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
-
-  const filteredBlogs =
-    selectedFilters.length > 0
-      ? BlogData.filter(
-          (blog) =>
-            blog.filters &&
-            blog.filters.some((filter) => selectedFilters.includes(filter))
-        )
-      : BlogData;
 
   const currentBlogs = filteredBlogs.slice(indexOfFirstBlog, indexOfLastBlog);
   return (
