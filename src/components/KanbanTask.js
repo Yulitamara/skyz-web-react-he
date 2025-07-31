0ceb3567836576138929c5db2b2c5e813b929c3a
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2025-07-31 13:31:53 +0300

    s

--- src/components/KanbanTask.js
+++ src/components/KanbanTask.js
@@ -8,37 +8,38 @@ const KanbanTask = ({ taskId, task, index }) => {
   if (!task) return null;
 
   return (
-    <Draggable draggableId={taskId} index={index}>
-      {(provided) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          className="kanban-task"
-        >
-          <div className="task-title-container">
-            <span>{task.titleKey ? t(task.titleKey) : task.title}</span>
-            {task.date && <span className="date">{task.date}</span>}
-          </div>
+ <Draggable draggableId={taskId} index={index}>
+  {(provided, snapshot) => (
+    <div
+      ref={provided.innerRef}
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+      className={`kanban-task ${snapshot.isDragging ? "dragging" : ""}`}
+    >
+      <div className="task-title-container">
+        <span>{task.titleKey ? t(task.titleKey) : task.title}</span>
+        {task.date && <span className="date">{task.date}</span>}
+      </div>
 
-          <div className="task-content">
-            {(task.descriptionKey || task.description) && (
-              <h3>{task.descriptionKey ? t(task.descriptionKey) : task.description}</h3>
-            )}
-            {(task.pKey || task.p) && (
-              <p>{task.pKey ? t(task.pKey) : task.p}</p>
-            )}
-          </div>
+      <div className="task-content">
+        {(task.descriptionKey || task.description) && (
+          <h3>{task.descriptionKey ? t(task.descriptionKey) : task.description}</h3>
+        )}
+        {(task.pKey || task.p) && (
+          <p>{task.pKey ? t(task.pKey) : task.p}</p>
+        )}
+      </div>
 
       <div className="task-title-container">
-  {task.groupKey && <span className="date">{t(task.groupKey)}</span>}
-  {(task.peopleKey || task.people) && (
-    <span>{task.peopleKey ? t(task.peopleKey) : task.people}</span>
+        {task.groupKey && <span className="date">{t(task.groupKey)}</span>}
+        {(task.peopleKey || task.people) && (
+          <span>{task.peopleKey ? t(task.peopleKey) : task.people}</span>
+        )}
+      </div>
+    </div>
   )}
-</div>
-        </div>
-      )}
-    </Draggable>
+</Draggable>
+
   );
 };
 
